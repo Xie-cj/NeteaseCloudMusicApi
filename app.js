ca3abb4730845f1d0f815a38311dd61bfa656e4b
Xie-cj <53166759+Xie-cj@users.noreply.github.com>
2021-04-29 19:47:14 +0800

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
     res.set({
       'Access-Control-Allow-Credentials': true,
 //       'Access-Control-Allow-Origin': req.headers.origin || '*',
-      'Access-Control-Allow-Origin': 'music.xcj.im',
+      'Access-Control-Allow-Origin': 'https://music.xcj.im',
       'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type',
       'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
       'Content-Type': 'application/json; charset=utf-8',
